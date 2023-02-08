ad2907df144b40dfb784f3305c6aea66fefc599f
Muaz Ashraf <muazashrafnet@gmail.com>
2023-02-09 02:59:48 +0500

    add header

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Header.module.css";
+import foodImage from "../../assets/food.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Meals by Muaz</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={foodImage} alt="food on table" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
