2cf9688f316e03e9622d46770bd967578ff27030
Muaz Ashraf <muazashrafnet@gmail.com>
2023-02-16 03:47:23 +0500

    add cart add and remove logic & animation

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,19 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/CartContext";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setbtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
-  const noOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  const { items } = cartCtx;
+  const noOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setbtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span>Your Order</span>
       <span className={styles.badge}>{noOfCartItems}</span>
     </button>
   );
